3b9c1126b8764b34f2462ff2e15156bc8c4cedea
shafali03 <shaf03sm@gmail.com>
2020-06-28 23:10:59 +0100

    feat: display one deposit transaction

--- src/account.js
+++ src/account.js
@@ -5,9 +5,14 @@ function Account() {
   this.transactions = ["date || credit || debit || balance"]
 }
 
+const todayDate = new Date();
+var date = (`${todayDate.getDate()}/${todayDate.getMonth() + [1]}/${todayDate.getFullYear()}`
+)
 
 Account.prototype.deposit = function (money) {
   this.balance += money
+  var record = `${date} || ${money} || || ${this.balance}`
+  this.transactions.splice(1, 0, record)
 }
 
 Account.prototype.withdraw = function (money) {
